3ce30555a78e35efcc3cfd65541d9eea1cc5bba2
Takahiro Ueda <t.ueda.od@juntendo.ac.jp>
2024-07-31 14:08:53 +0900

    fix: allow adding non-existent systematics in _parse_sample()

--- madminer/lhe/lhe_reader.py
+++ madminer/lhe/lhe_reader.py
@@ -701,9 +701,7 @@ class LHEReader:
             for nuisance_param_name, ((benchmark0, weight0), (benchmark1, weight1), _) in nuisance_info.items():
                 nuisance_param = self.nuisance_parameters.get(nuisance_param_name)
 
-                if nuisance_param is None:
-                    raise RuntimeError(f"Nuisance parameter {nuisance_param_name} does not exist")
-                if (
+                if nuisance_param and (
                     nuisance_param.systematic != systematics_name
                     or nuisance_param.benchmark_pos != benchmark0
                     or nuisance_param.benchmark_neg != benchmark1
